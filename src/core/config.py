cd25ff047343a5649640d317a247d75f59e4f6fc
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-08 15:07:40 +0300

    create core/config with project settings

---
+++ src/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, PostgresDsn
+from functools import lru_cache
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv(".env"))
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DB_ECHO: bool  # Временно true;
+
+    @property
+    def db_url(self) -> PostgresDsn:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
