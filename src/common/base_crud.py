02ce8c491802720d058c15e646094274b97c0372
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-08 15:16:19 +0300

    create common/base_crud and common/base_dependecies

---
+++ src/common/base_crud.py
@@ -0,0 +1,47 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, Result
+from sqlalchemy.orm import DeclarativeBase
+from typing import TypeVar, Type
+from pydantic import BaseModel
+
+
+T = TypeVar("T", bound=DeclarativeBase)  # ?
+D = TypeVar('D', bound=BaseModel)  # ?
+
+
+async def create_item(session: AsyncSession, model: Type[T], data: D) -> T:
+    item = model(**data.model_dump())
+    session.add(item)
+    await session.commit()
+
+    return item
+
+
+async def get_item(session: AsyncSession, model: Type[T], item_id: int) -> T | None:
+    return await session.get(model, item_id)
+
+
+async def get_items(session: AsyncSession, model: Type[T]) -> list[T]:
+    stmt = select(model).order_by(model.id)
+    result: Result = await session.execute(stmt)
+    items = result.scalars().all()
+
+    return list(items)
+
+
+async def update_item(
+        session: AsyncSession,
+        item: T,
+        data: D,
+        partial: bool = False
+) -> T:
+    for name, value in data.model_dump(exclude_unset=partial).items():
+        setattr(item, name, value)
+    await session.commit()
+
+    return item
+
+
+async def delete_item(session: AsyncSession, item: T) -> None:
+    await session.delete(item)
+    await session.commit()
---
+++ src/common/base_dependencies.py
@@ -0,0 +1,39 @@
+from typing import Type, TypeVar, Callable
+from fastapi import Depends, HTTPException, status, Path
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.database import get_database
+from sqlalchemy.orm import DeclarativeBase
+from typing import Annotated
+from common.base_crud import get_item
+
+
+T = TypeVar("T", bound=DeclarativeBase)
+
+
+# async def get_item_by_id(
+#         item_id: Annotated[int, Path],
+#         model: Type[T],
+#         session: AsyncSession = Depends(get_database().session_dependency)
+# ) -> T:
+#     item = await get_item(session=session, model=model, item_id=item_id)
+#     if item:
+#         return item
+#     raise HTTPException(
+#         status_code=status.HTTP_404_NOT_FOUND,
+#         detail=f"{model.__name__} {item_id} not found!"
+#     )
+
+
+def get_item_by_id(model: Type[T]) -> Callable:
+    async def dependency(
+        item_id: Annotated[int, Path],
+        session: AsyncSession = Depends(get_database().session_dependency)
+    ) -> T:
+        item = await get_item(session=session, model=model, item_id=item_id)
+        if item:
+            return item
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"{model.__name__} {item_id} not found!"
+        )
+    return dependency
