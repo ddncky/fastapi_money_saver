86365991f322647fd3816a2db2ac589355236a9b
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-08 21:01:27 +0300

    create migration for users tables

--- src/modules/users/user.py
+++ src/modules/users/user.py
@@ -1,4 +1,4 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from src.core.base import Base
 from src.common.mixins.int_id_pk import IntIdPkMixin
 
