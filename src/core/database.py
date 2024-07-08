9f221b4bd4f7ffdd6a46b186409fd154cb0939d1
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-08 15:07:28 +0300

    create core/database

---
+++ src/core/database.py
@@ -0,0 +1,53 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+    async_scoped_session)
+from src.core.config import get_settings
+from functools import lru_cache
+from typing import Iterator
+
+
+class DatabaseHelper:
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autocommit=False,
+            autoflush=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> Iterator[AsyncSession]:
+        # async with self.session_factory() as session:  Так тоже можно;
+        #     try:
+        #         yield session
+        #     finally:
+        #         await session.close()
+        session = self.get_scoped_session()
+        try:
+            yield session
+        finally:
+            await session.remove()
+
+
+@lru_cache
+def get_database() -> DatabaseHelper:
+    db_helper = DatabaseHelper(
+        url=get_settings().db_url,
+        echo=get_settings().DB_ECHO
+    )
+    return db_helper
+
