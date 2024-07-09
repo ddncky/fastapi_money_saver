4985ebbfe0d6fcaebbef9a2b4ce8a47b65ad8897
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-09 17:36:41 +0300

    fixed router tag

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from .auth_routers.auth_router import router as auth_router
 from .auth_routers.users_router import router as users_router
 
-router = APIRouter(prefix="/api", tags=["Auth"])
+router = APIRouter(prefix="/api")
 router.include_router(auth_router)
 router.include_router(users_router)
 
