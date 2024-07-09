b93c1208f25e091a9ddce30cbab7f0ea3d98dccf
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-09 11:51:03 +0300

    create authentication backend

---
+++ src/api/dependencies/authentication/backend.py
@@ -0,0 +1,10 @@
+from fastapi_users.authentication import AuthenticationBackend
+from src.core.authentication.transport import bearer_transport
+from .strategy import get_database_strategy
+
+
+authentication_backend = AuthenticationBackend(
+    name="accesstokens",
+    transport=bearer_transport,
+    get_strategy=get_database_strategy,
+)
