69add7d36f95dc14de03353c0361318af7ac4406
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-09 11:44:11 +0300

    create db strategy

---
+++ src/api/dependencies/authentication/strategy.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING, Annotated
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import DatabaseStrategy
+
+from src.core import get_settings
+
+from .access_tokens import get_access_token_db
+
+if TYPE_CHECKING:
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+
+    from src.core import AccessToken
+
+
+def get_database_strategy(
+    access_token_db: Annotated["AccessTokenDatabase[AccessToken]", Depends(get_access_token_db)],
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        database=access_token_db,
+        lifetime_seconds=get_settings().access_token_lifetime_seconds
+    )
