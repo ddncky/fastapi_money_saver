b727a08db6cfdaf9b66b0ad56670c71095cac657
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-09 20:07:44 +0300

    add type hinting to database

--- src/core/models/database.py
+++ src/core/models/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
     AsyncSession,
-    async_scoped_session)
+    async_scoped_session,
+    AsyncEngine)
 from src.core.config import get_settings
 from functools import lru_cache
 from typing import Iterator
@@ -12,7 +13,7 @@ from typing import Iterator
 
 class DatabaseHelper:
     def __init__(self, url: str, echo: bool = False):
-        self.engine = create_async_engine(
+        self.engine: AsyncEngine = create_async_engine(
             url=url,
             echo=echo,
         )
