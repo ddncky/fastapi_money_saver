85dfcb0db182aeda3df11715c5f07941a88589d6
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-10 21:18:01 +0300

    create messages file for example

---
+++ src/api/messages.py
@@ -0,0 +1,42 @@
+from typing import Annotated
+
+from fastapi import APIRouter
+from fastapi import Depends
+
+from src.api.auth_routers.fastapi_users_router import (
+    current_active_user,
+    current_active_superuser,
+)
+from src.modules import User
+from src.modules.users.schemas import UserRead
+
+router = APIRouter(
+    prefix="/messages",
+    tags=["Messages"],
+)
+
+
+@router.get("")
+def get_user_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return {
+        "messages": ["m1", "m2", "m3"],
+        "user": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets")
+def get_superuser_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    return {
+        "messages": ["secret-m1", "secret-m2", "secret-m3"],
+        "user": UserRead.model_validate(user),
+    }
