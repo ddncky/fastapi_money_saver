a472722e1b6918e6f44f095ac07fbd7d10aa3766
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-11 10:50:01 +0300

    update user model with created_at and relationship

--- src/modules/users/user.py
+++ src/modules/users/user.py
@@ -1,4 +1,9 @@
+import datetime
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import DateTime
+
 from src.core import Base
 from src.common.mixins import IntIdPkMixin
 from typing import TYPE_CHECKING
@@ -7,9 +12,13 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from src.modules import Account
 
 
 class User(Base, IntIdPkMixin, SQLAlchemyBaseUserTable[int]):
+    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.datetime.now(datetime.UTC))
+
+    accounts: Mapped[list["Account"]] = relationship("Account", back_populates="users")
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
