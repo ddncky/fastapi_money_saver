dd6e91a58f688793cad59a51bbb0ef1e5e1c326f
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-12 13:57:31 +0300

    Include categories and transactions routers to modules router;

--- src/modules/router.py
+++ src/modules/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from src.modules.accounts.views import router as account_router
-
-
+from .accounts.views import router as account_router
+from .categories.views import router as categories_router
+from .transactions.views import router as transactions_router
 
 modules_router = APIRouter()
 modules_router.include_router(account_router)
+modules_router.include_router(categories_router)
+modules_router.include_router(transactions_router)
