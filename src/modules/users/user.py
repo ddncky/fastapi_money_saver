1aae893d7fcfb41ec2b5f2f2e7647d75e67c0296
Dmytro Dudnytskyi <montekristo705@gmail.com>
2024-07-12 13:45:50 +0300

    TODO is gone;

--- src/modules/users/user.py
+++ src/modules/users/user.py
@@ -8,8 +8,6 @@ from src.core import Base
 from src.common.mixins import IntIdPkMixin
 from typing import TYPE_CHECKING
 
-# TODO: переименовать этот файл в модель или переместить его потом;
-
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
